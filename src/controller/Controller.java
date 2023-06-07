acf0550d2a70122c91feb5890dc0573bd19f1466
Coleen Agsao <caagsao@up.edu.ph>
2023-06-08 00:12:44 +0800

    feat: add functionalities of single player mode first in server for now

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,13 +1,24 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 import javafx.application.Platform;
@@ -32,6 +43,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import model.NavState;
 import model.User;
@@ -115,6 +127,233 @@ public class Controller {
 
 	//Game
 	@FXML private VBox vboxGame;
+	private int wordCounter = 0;
+	private int first = 1;
+	private int win = 0;
+	private double finishX;
+    private File saveData;
+    ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+
+    @FXML public Text seconds;
+	@FXML private Text programWord;
+	@FXML private Text secondProgramWord;
+	@FXML private Text thirdProgramWord;
+	@FXML private Text fourthProgramWord;
+    @FXML private TextField userWord;
+    @FXML private ImageView correct;
+    @FXML private ImageView car;
+    @FXML private ImageView finish;
+    @FXML private ImageView wrong;
+    @FXML private Button playAgain;
+
+    @FXML
+	private void moveCarRight() {
+	    car.setLayoutX(car.getLayoutX() + 50);
+	}
+
+    ArrayList<String> words = new ArrayList<String>();
+
+    // add words to array list
+    public void addToList() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new
+                    FileReader("wordsList"));
+            String line = reader.readLine();
+            while (line != null) {
+                words.add(line);
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initializeGame(ActionEvent event) {
+
+		playAgain.setVisible(false);
+        playAgain.setDisable(true);
+        finishX = finish.getLayoutX();
+        seconds.setText("60");
+        addToList();
+        Collections.shuffle(words);
+        programWord.setText(words.get(wordCounter));
+        secondProgramWord.setText(words.get(wordCounter+1));
+        thirdProgramWord.setText(words.get(wordCounter+2));
+        fourthProgramWord.setText(words.get(wordCounter+3));
+        wordCounter++;
+
+//        Date date = new Date();
+//        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss");
+//        saveData = new File("src/data/"+formatter.format(date).trim()+".txt");
+
+//        try {
+//            if (saveData.createNewFile()) {
+//                System.out.println("File created: " + saveData.getName());
+//            } else {
+//                System.out.println("File already exists.");
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+	}
+
+    private int timer = 60;
+
+	Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            if (timer > -1) {
+                seconds.setText(String.valueOf(timer));
+                timer -= 1;
+            }
+
+            else {
+                if (timer == -1) {
+                	if (win == 0){
+                    	userWord.setDisable(true);
+                        userWord.setText("Game over. You lose!");
+                        timer = -4;
+                	} else {
+                    	userWord.setDisable(true);
+                        userWord.setText("Game over. You win!");
+                        timer = -4;
+                        System.out.println(timer);
+                	}
+
+//                    try {
+//                        FileWriter myWriter = new FileWriter(saveData);
+//                        myWriter.write(countAll +";");
+//                        myWriter.write(counter +";");
+//                        myWriter.write(String.valueOf(countAll-counter));
+//                        myWriter.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+                }
+
+                if (timer == -4) {
+                    playAgain.setVisible(true);
+                    playAgain.setDisable(false);
+                    executor.shutdown();
+                }
+
+                timer -= 1;
+            }
+        }
+    };
+
+    public void backtoMenu(ActionEvent event){
+    	this.vboxGame.setVisible(false);
+    	this.vboxMP.setVisible(true);
+    	this.vboxMP.setDisable(false);
+    }
+
+    Runnable fadeCorrect = new Runnable() {
+        @Override
+        public void run() {
+            correct.setOpacity(0);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            correct.setOpacity(50);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            correct.setOpacity(100);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            correct.setOpacity(0);
+        }
+    };
+
+    Runnable fadeWrong = new Runnable() {
+        @Override
+        public void run() {
+            wrong.setOpacity(0);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            wrong.setOpacity(50);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            wrong.setOpacity(100);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            wrong.setOpacity(0);
+        }
+    };
+
+
+
+    private int countAll = 0;
+    private int counter = 0;
+
+
+    public void startType(KeyEvent ke) {
+        // only gets called once
+        if (first == 1) {
+            first = 0;
+            executor.scheduleAtFixedRate(r, 0, 1, TimeUnit.SECONDS);
+
+        }
+
+
+        if (ke.getCode().equals(KeyCode.ENTER)) {
+
+            String s = userWord.getText().trim();
+            String real = programWord.getText();
+            countAll++;
+
+            // if correct
+            if (s.equals(real)) {
+                counter++;
+                //wordsPerMin.setText(String.valueOf(counter));
+                Thread t = new Thread(fadeCorrect);
+                t.start();
+                moveCarRight();
+
+                System.out.println(finishX + "vs" + car.getLayoutX());
+
+                if ((finish.getLayoutX() - 101) <= car.getLayoutX()){
+                    win = 1;
+                	timer = -1;
+                }
+            }
+
+            else {
+                Thread t = new Thread(fadeWrong);
+                t.start();
+            }
+            userWord.setText("");
+            //accuracy.setText(String.valueOf(Math.round((counter*1.0/countAll)*100)));
+            programWord.setText(words.get(wordCounter));
+            secondProgramWord.setText(words.get(wordCounter+1));
+            thirdProgramWord.setText(words.get(wordCounter+2));
+            fourthProgramWord.setText(words.get(wordCounter+3));
+            wordCounter++;
+        }
+
+    }
+
 
 	private int connectedUsers;
 
@@ -553,6 +792,7 @@ public class Controller {
 	{
 		System.out.println("Start game");
 		this.vboxGame.setVisible(true); //temp
+		this.initializeGame(event);
 	}
 
 	// MultiPlayer: Client callbacks
